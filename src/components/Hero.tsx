37473627791f49af15ac9d1b108bc46827b757c2
Bruno Moreira <bruno@terraduo.com>
2024-05-22 15:26:03 -0600

    Improves layout of landing page.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
             <div className="mx-auto container px-4 md:px-6">
                 <div className="flex flex-col space-y-8 lg:space-y-0 lg:flex-row items-center lg:space-x-12 xl:space-x-24 2xl:space-x-32 text-center lg:text-left">
-                    <div className="lg:flex-1 md:text-3xl font-body md:leading-snug text-shadow shadow-purple-800">
-                        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-8xl text-white font-title">sheet.stream</h1>
+                    <div className="text-2xl md:text-3xl font-body md:leading-snug text-shadow shadow-purple-800">
+                        <h1 className="text-4xl font-bold tracking-tighter md:text-5xl xl:text-7xl 2xl:text-8xl text-white font-title">sheet.stream</h1>
                         <p className="mt-4">Display data from a Google Sheet on your Twitch/YouTube/TikTok streams in real-time.</p>
                         <p className="mt-4">Free, easy to setup, and customizable.</p>
                         <div className="mt-8">
@@ -24,8 +24,17 @@ export default function Hero() {
                             </Link>
                         </div>
                     </div>
-                    <div className="">
-                        <Image alt="Hero Image" priority={true} width={730} height={612} src={heroImage} />
+                    <div className="w-1/2 lg:flex-shrink-0">
+                        <Image
+                            sizes="100vw"
+                            style={{
+                                width: "100%",
+                                height: "auto",
+                            }}
+                            alt="Hero Image"
+                            priority={true}
+                            src={heroImage}
+                        />
                     </div>
                 </div>
             </div>
